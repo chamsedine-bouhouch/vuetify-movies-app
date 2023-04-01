7d091195791b92c4b447b282ba030c933af2c799
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-04-01 14:20:48 +0200

    initial commit

---
+++ src/layouts/default/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <v-footer border>
+  <div class="d-flex ">
+    <span class="align-center justify-center">
+      &copy;
+      {{ new Date().getFullYear() }}
+      Made By
+      <a
+        href="https://www.linkedin.com/in/chamseddine-bouhouch/"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-primary ms-1"
+        >Chamseddine Bouhouch</a
+      >
+    </span></div>
+    </v-footer> --><v-footer class="bg-black">
+    <v-row justify="center" no-gutters>
+  
+      <v-col class="text-center mt-4" cols="12">
+        {{ new Date().getFullYear() }} — <strong>   
+       
+     Chamseddine Bouhouch </strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+ <CardList/>
+</template>
+
+<script lang="ts" setup>
+import CardList from '@/components/CardList.vue';
+
+ </script>
